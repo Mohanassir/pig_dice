9baf88d1ceb85e7ad8635d8cf86ba945735333c1
Mohanassir <iamnasir10k@gmail.com>
2019-03-04 09:09:54 +0300

    updated README.md file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function rollDice(){
   var dice1=document.getElementById("dice1");
   var dice2=document.getElementById("dice1");
-
+  var status=document.getElelementById("status");
   var d1= math.floor(math.random() * 6) + 1;
   var d2= math.floor(math.random() * 6) + 1;
   var diceTotal = d1 + d2;
 
   dice1.innerHTML = d1;
   dice2.innerHTML = d2;
+  status.innerHTML= "You rolled "+diceTotal+".";
+  
 }
